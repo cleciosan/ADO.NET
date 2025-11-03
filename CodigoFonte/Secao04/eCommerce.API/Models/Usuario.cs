5154b6406311b1f731a86e34df233ca6e65ea702
Clecio Batista Santos <cleciosan@hotmail.com>
2025-11-03 11:49:39 -0300

    Initialize projetc, create folder Models, create Class Usuario

---
+++ CodigoFonte/Secao04/eCommerce.API/Models/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace eCommerce.API.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+    }
+}
