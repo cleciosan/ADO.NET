ba1e7e09d176c44a18f87ab6d0587d7a0b56cdb9
Clecio Batista Santos <cleciosan@hotmail.com>
2025-11-04 09:20:55 -0300

    Create Database, alter model Usuario, create model Contato

--- CodigoFonte/Secao04/eCommerce.API/Models/Usuario.cs
+++ CodigoFonte/Secao04/eCommerce.API/Models/Usuario.cs
@@ -7,5 +7,17 @@
         public string Nome { get; set; }
 
         public string Email { get; set; }
+
+        public string Sexo { get; set; }
+
+        public string RG { get; set; }
+
+        public string CPF { get; set; }
+
+        public string NomeMae { get; set; }
+
+        public string SituacaoCadastro { get; set; }
+
+        public DateTimeOffset DataCadastro { get; set; }
     }
 }
