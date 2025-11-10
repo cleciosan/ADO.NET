902c9629bcdc5391a2c4c014254c2f82532ba95f
Clecio Batista Santos <cleciosan@hotmail.com>
2025-11-10 11:38:10 -0300

    Implement relationship between Users and Contacts; Implement relationship between Users and Departments

--- CodigoFonte/Secao04/eCommerce.API/Repositories/UsuarioRepository.cs
+++ CodigoFonte/Secao04/eCommerce.API/Repositories/UsuarioRepository.cs
@@ -56,7 +56,99 @@ namespace eCommerce.API.Repositories
         {
             try
             {
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "SELECT * FROM USUARIOS u " +
+                    "LEFT JOIN Contatos c ON c.UsuarioId = u.Id " +
+                    "LEFT JOIN EnderecosEntrega ee ON ee.UsuarioId = u.Id " +
+                    "WHERE u.Id = @Id";
+                command.Parameters.AddWithValue("@Id", id);
+                command.Connection = (SqlConnection)_connection;
+
                 _connection.Open();
+                SqlDataReader dataReader = command.ExecuteReader();
+
+                Dictionary<int, Usuario> usuarios = new Dictionary<int, Usuario>();
+
+                while (dataReader.Read())
+                {
+                    Usuario usuario = new Usuario();
+                    if (!(usuarios.ContainsKey(dataReader.GetInt32(0))))
+                    {
+                        usuario.Id = dataReader.GetInt32("Id");
+                        usuario.Nome = dataReader.GetString("Nome");
+                        usuario.Email = dataReader.GetString("Email");
+                        usuario.Sexo = dataReader.GetString("Sexo");
+                        usuario.RG = dataReader.GetString("RG");
+                        usuario.CPF = dataReader.GetString("CPF");
+                        usuario.NomeMae = dataReader.GetString("NomeMae");
+                        usuario.SituacaoCadastro = dataReader.GetString("SituacaoCadastro");
+                        usuario.DataCadastro = dataReader.GetDateTimeOffset(8);
+
+                        Contato contato = new Contato();
+                        contato.Id = dataReader.GetInt32(9);
+                        contato.UsuarioId = usuario.Id;
+                        contato.Telefone = dataReader.GetString("Telefone");
+                        contato.Celular = dataReader.GetString("Celular");
+
+                        usuario.Contato = contato;
+
+                        usuarios.Add(usuario.Id, usuario);
+                    }
+                    else
+                    {
+                        usuario = usuarios[dataReader.GetInt32(0)];
+                    }
+                    
+
+                    EnderecoEntrega enderecoEntrega = new EnderecoEntrega();
+                    enderecoEntrega.Id = dataReader.GetInt32(13);
+                    enderecoEntrega.UsuarioId = usuario.Id;
+                    enderecoEntrega.NomeEndereco = dataReader.GetString("NomeEndereco");
+                    enderecoEntrega.CEP = dataReader.GetString("CEP");
+                    enderecoEntrega.Estado = dataReader.GetString("Estado");
+                    enderecoEntrega.Cidade = dataReader.GetString("Cidade");    
+                    enderecoEntrega.Bairro = dataReader.GetString("Bairro");
+                    enderecoEntrega.Endereco = dataReader.GetString("Endereco");
+                    enderecoEntrega.Numero = dataReader.GetString("Numero");
+                    enderecoEntrega.Complemento = dataReader.GetString("Complemento");
+
+                    usuario.EnderecosEntrega = (usuario.EnderecosEntrega == null) ? new List<EnderecoEntrega>() : usuario.EnderecosEntrega;
+                    usuario.EnderecosEntrega.Add(enderecoEntrega);
+
+                    
+                }
+
+                return usuarios[usuarios.Keys.First()];
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+            return null;
+        }
+
+        public void Insert(Usuario usuario)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "INSERT INTO USUARIOS (Nome, Email, Sexo, RG, CPF, NomeMae, SituacaoCadastro, DataCadastro) "+
+                                      "VALUES (@Nome, @Email, @Sexo, @RG, @CPF, @NomeMae, @SituacaoCadastro, @DataCadastro);"+
+                                      "SELECT CAST(scope_identity() AS int)";
+                command.Connection = (SqlConnection)_connection;
+
+                command.Parameters.AddWithValue("@Nome", usuario.Nome);
+                command.Parameters.AddWithValue("@Email", usuario.Email);
+                command.Parameters.AddWithValue("@Sexo", usuario.Sexo);
+                command.Parameters.AddWithValue("@RG", usuario.RG);
+                command.Parameters.AddWithValue("@CPF", usuario.CPF);
+                command.Parameters.AddWithValue("@NomeMae", usuario.NomeMae);
+                command.Parameters.AddWithValue("@SituacaoCadastro", usuario.SituacaoCadastro);
+                command.Parameters.AddWithValue("@DataCadastro", usuario.DataCadastro);
+
+                _connection.Open();
+                usuario.Id = (int)command.ExecuteScalar();
             }
             finally
             {
@@ -64,42 +156,57 @@ namespace eCommerce.API.Repositories
             }
         }
 
-        public void Insert(Usuario usuario)
-        {
-            var ultimoUsuario = _db.LastOrDefault();
-
-            if(ultimoUsuario == null)
-            {
-                usuario.Id = 1;
-            }
-            else
-            {
-                usuario.Id = ultimoUsuario.Id;
-                usuario.Id++;
-            }
-
-            _db.Add(usuario);
-        }
-
         public void Update(Usuario usuario)
         {
-            _db.Remove(_db.FirstOrDefault(a => a.Id == usuario.Id));
-            _db.Add(usuario);
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "UPDATE USUARIOS SET " +
+                                      "Nome = @Nome, Email = @Email, Sexo = @Sexo, RG = @RG, " +
+                                      "CPF = @CPF, NomeMae = @NomeMae, SituacaoCadastro = @SituacaoCadastro, " +
+                                      "DataCadastro = @DataCadastro " +
+                                      "WHERE Id = @Id";
+                command.Connection = (SqlConnection)_connection;
+
+                command.Parameters.AddWithValue("@Nome", usuario.Nome);
+                command.Parameters.AddWithValue("@Email", usuario.Email);
+                command.Parameters.AddWithValue("@Sexo", usuario.Sexo);
+                command.Parameters.AddWithValue("@RG", usuario.RG);
+                command.Parameters.AddWithValue("@CPF", usuario.CPF);
+                command.Parameters.AddWithValue("@NomeMae", usuario.NomeMae);
+                command.Parameters.AddWithValue("@SituacaoCadastro", usuario.SituacaoCadastro);
+                command.Parameters.AddWithValue("@DataCadastro", usuario.DataCadastro);
+
+                command.Parameters.AddWithValue("@Id", usuario.Id);
+
+                _connection.Open();
+                command.ExecuteNonQuery();
+            }
+            finally
+            {
+                _connection.Close();
+            }
         }
 
         public void Delete(int id)
         {
-            _db.Remove(_db.FirstOrDefault(a => a.Id == id));
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "DELETE FROM USUARIOS WHERE Id = @Id";
+                command.Connection = (SqlConnection)_connection;
+                
+                command.Parameters.AddWithValue("@Id", id);
+
+                _connection.Open();
+                command.ExecuteNonQuery();
+            }
+            finally
+            {
+                _connection.Close();
+            }
         }
 
-
-
-        private static List<Usuario> _db = new List<Usuario>()
-        {
-            new Usuario() { Id = 1, Nome = "Filipe Rodrigues", Email = "filipe.rodrigues@gmail.com" },
-            new Usuario() { Id = 2, Nome = "Marcelo Rodrigues", Email = "marcelo.rodrigues@gmail.com" },
-            new Usuario() { Id = 3, Nome = "Jessica Rodrigues", Email = "jessica.rodrigues@gmail.com" }
-        };
 
     }
 }
